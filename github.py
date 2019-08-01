72bfb462812d9d7229e901c269b73f1d201c7afe
Mayara Melo <mayara@sejainova.com>
2019-08-01 19:23:11 -0300

    implement github methods to get user profile and repos

--- github.py
+++ github.py
@@ -3,14 +3,42 @@ import requests
 
 
 class GithubConsultant:
-    host = 'github.com'
-
+    URL_BASE = "api.github.com/users/"
+    # headers = {"Accept": "application/vnd.github.v3+json"}
     # def __init__(self):
     #     self.host = 'github.com'
+
+    @staticmethod
+    def _get_filtered_response(keys, response):
+        attrs = {}
+        for k in keys:
+            value = response.get(k, None)
+            attrs[k] = "" if value is None else value
+        return attrs
+
     @staticmethod
     def get_user_profile(username):
-        return {}
+        url = f'https://api.github.com/users/{username}'
+        response = requests.get(url)
+        fields = ["id", "login", "name", "avatar_url", "html_url"]
+        if response.status_code == 200:
+            return GithubConsultant._get_filtered_response(fields, response.json())
+        else:
+            return 1
 
     @staticmethod
     def get_user_repos(username):
-        return {}
+        url = f'https://api.github.com/users/{username}/repos'
+        response = requests.get(url)
+        if response.status_code == 200:
+            fields = [
+                "id",
+                "name",
+                "description",
+                "html_url"
+            ]
+            repos = response.json()
+            return [GithubConsultant._get_filtered_response(
+                fields, r) for r in repos]
+        else:
+            return
--- test.py
+++ test.py
@@ -3,23 +3,53 @@ from github import GithubConsultant
 
 
 class TestGithubConsultant(unittest.TestCase):
-    def test_get_user_profile(self):
+    def test_get_valid_user_profile(self):
         """
         Test getting user profile
         """
-        user_profiles = GithubConsultant.get_user_profile(
+        user_profile = GithubConsultant.get_user_profile(
             username='mayara-melo')
+        self.assertDictEqual(user_profile, {
+            "id": 6036370,
+            "login": "mayara-melo",
+            "name": "",
+            "avatar_url": "https://avatars3.githubusercontent.com/u/6036370?v=4",
+            "html_url": "https://github.com/mayara-melo"
+        })
 
-        self.assertIsInstance(user_profiles, dict)
-
-    def test_get_user_repos(self):
+    def test_get_valid_user_repos(self):
         """
         Test getting user repos
         """
-        user_profiles = GithubConsultant.get_user_repos(
+        user_repos = GithubConsultant.get_user_repos(
             username='mayara-melo')
 
-        self.assertIsInstance(user_profiles, dict)
+        self.assertCountEqual(user_repos, [
+            {
+                "id": 31819396,
+                "name": "analise-juridica",
+                "html_url": "https://github.com/mayara-melo/analise-juridica",
+                "description": ""
+            },
+            {
+                "id": 44837872,
+                "name": "juridicNetworks",
+                "html_url": "https://github.com/mayara-melo/juridicNetworks",
+                "description": ""
+            },
+            {
+                "id": 27124419,
+                "name": "projeto_sbc",
+                "html_url": "https://github.com/mayara-melo/projeto_sbc",
+                "description": "Projeto da disciplina Sistemas Baseados em Conhecimento"
+            },
+            {
+                "id": 199935279,
+                "name": "test-python-webservice",
+                "html_url": "https://github.com/mayara-melo/test-python-webservice",
+                "description": ""
+            }
+        ])
 
 
 if __name__ == '__main__':
